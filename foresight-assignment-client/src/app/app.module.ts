0c5d497ba8f9f8aa5f352f700448df7323dd7904
shashiradw <51243063+shashiradw@users.noreply.github.com>
2021-10-09 13:50:56 +0530

    Login Ui done

--- foresight-assignment-client/src/app/app.module.ts
+++ foresight-assignment-client/src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SigninComponent } from './componenets/signin/signin.component';
+import { FormComponent } from './componenets/form/form.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    FormComponent,
   ],
   imports: [
     BrowserModule,
